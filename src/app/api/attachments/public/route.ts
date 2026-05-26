ce7cc7fdd6798c8e2f45e79bee0d2f25f426cdb0
priosshrsth <shrsthprios@hotmail.com>
2026-05-26 09:22:52 +0000

    remove unused imports

--- src/app/api/attachments/public/route.ts
+++ src/app/api/attachments/public/route.ts
@@ -1,5 +1,4 @@
 import { withErrorHandler } from '@api/core/utils/withErrorHandler'
-import { withExecTimeCap } from '@api/core/utils/withExecTimeCap'
 import { createAttachmentPublic } from '@api/attachments/public/public.controller'
 
 export const maxDuration = 300
